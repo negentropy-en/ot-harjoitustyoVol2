f1d29e35f69d9d36afe5a2307f978e05fa957869
negentropy-en <patrick.r.o.grahn@gmail.com>
2026-03-24 20:57:57 +0200

    Added additional maksukortti tests task 3

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -28,3 +28,22 @@ class TestMaksukortti(unittest.TestCase):
     def test_kortin_saldo_ei_ylita_maksimiarvoa(self):
         self.kortti.lataa_rahaa(20000)
         self.assertEqual(self.kortti.saldo_euroina(), 150.0)
+        
+    def test_syo_maukkaasti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(300)
+        kortti.syo_maukkaasti()
+        self.assertEqual(kortti.saldo_euroina(), 3.0)
+
+    def test_negatiivisen_summan_lataaminen_ei_muuta_saldoa(self):
+        self.kortti.lataa_rahaa(-500)
+        self.assertEqual(self.kortti.saldo_euroina(), 10.0)
+
+    def test_kortilla_voi_ostaa_edullisen_kun_saldo_juuri_riittaa(self):
+        kortti = Maksukortti(250)
+        kortti.syo_edullisesti()
+        self.assertEqual(kortti.saldo_euroina(), 0.0)
+
+    def test_kortilla_voi_ostaa_maukkaan_kun_saldo_juuri_riittaa(self):
+        kortti = Maksukortti(400)
+        kortti.syo_maukkaasti()
+        self.assertEqual(kortti.saldo_euroina(), 0.0)
